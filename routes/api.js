ab9c92d72b7ba0abbddf5d3a8d793b47fbb6c2b4
QJNel <quintinnel9@gmail.com>
2020-11-22 10:59:07 +0200

    Added file upload to routes

--- routes/api.js
+++ routes/api.js
@@ -123,7 +123,7 @@ router.post('/upload', (req, res) => {
 
   const file = req.files.file;
 
-  file.mv(`../mernApp/client/public/uploads/${file.name}`, err => {
+  file.mv(`${__dirname}/public/uploads/${file.name}`, err => {
     if (err) {
       console.error(err);
       return res.status(500).send(err);
